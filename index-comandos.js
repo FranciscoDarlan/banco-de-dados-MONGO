8f0a46a0905f3a7ac4d4766dc470fdc0ac7fceb7
Francisco Darlan <118319737+FranciscoDarlan@users.noreply.github.com>
2024-06-26 15:18:45 -0300

    Update index-comandos.js

--- index-comandos.js
+++ index-comandos.js
@@ -1,1 +1,34 @@
 COMANDOS BANCO DE DADOS MONGO DB
+
+criando nosso primeiro banco de dados!
+
+vamos no site mongodb.com
+clica no tryfree(testegratis)
+na tela de cadastro tem 2 opções: logar com goolge ou digitar email senha etc..
+
+entrei com email do google mesmo !
+
+aceitar os termos ..
+welcome!!!
+
+vai aparecer uma pesquisa interna, qual seu objetivo !
+ vai para tela de pagamentos
+queremos o gratuito! Mzero - M0
+nome: do banco o meu vou criar com (USERS)
+vai perguntar qual localidade, quanto mais perto colocar vai funcionar mais rapido!
+temos somente o brazil/ são paulo
+click em craete deployme
+
+vai perguntar o username com password
+coloca seu nome e senha 
+guarda o nome e senha do banco de dados
+click crate user
+click closecriate
+
+JÁ CRIOU O BANCO DE DADOS
+_______________________________________________________________________-
+ SÓ QUE O BANCO DE DADOS SOMENTE ESTÁ NO SITE, temos que conectar ao nosso 
+codigo/projeto node!
+para facilitar, não vamos fazer tudo do zero 
+  vamos utilizar biblioteca para fazer essa conexão!
+
